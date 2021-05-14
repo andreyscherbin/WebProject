13e4e61e17d3421b26600418685450c697436df6
andreyscherbin <andrey123scherbin@gmail.com>
2021-05-14 09:04:12 +0400

    update

--- FinalProject/src/main/java/com/epam/forum/tag/function/IsAuthenticatedFunction.java
+++ FinalProject/src/main/java/com/epam/forum/tag/function/IsAuthenticatedFunction.java
@@ -9,7 +9,7 @@ public class IsAuthenticatedFunction {
 	private static final String ATTRIBUTE_NAME_ROLE = "role";
 
 	public static Boolean isAuthenticated(JspContext jspContext) {
-		PageContext pageContext = (PageContext) jspContext; // need to think why
+		PageContext pageContext = (PageContext) jspContext;
 		HttpSession session = pageContext.getSession();
 		String stringRole = (String) session.getAttribute(ATTRIBUTE_NAME_ROLE);
 		Role role = Role.valueOf(stringRole);
